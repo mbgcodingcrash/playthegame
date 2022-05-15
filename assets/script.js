73cb4cf9dadfc6bdd185085521df0d7862169bc4
mbgcodingcrash <re4u2c@gmail.com>
2022-05-15 13:31:19 -0400

    set up some html, some css, and started the javascript

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+const startButton = document.getElementById('startBtn')
+startButton.addEventListener('click', startGame)
+
+
+
+function startGame(){
+
+}
+
+function setNextQuestion(){
+
+}
+
+
+
+const questions = [
+    {
+        question: 'Is coding fun?'
+        answers: [
+            {text: 'Yes', correct: true }
+            {text: 'No', correct: false }
+            {text: 'Maybe?', correct: false }
+            {text: 'Definately Maybe', correct: false }
+        ]
+
+    }
+]
