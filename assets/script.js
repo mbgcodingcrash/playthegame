44b90d10833cc49ce9aaf053d7ad2381a1366e99
mbgcodingcrash <re4u2c@gmail.com>
2022-05-15 17:33:57 -0400

    banging head against wall. Got the start button to go to answer 1, and to remove the placeholder text for the question, however i can not get the actual question to load there yet

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,46 @@
+//Variables for the quiz
 var startTime = 100;
-
-
-
 const questionBox =document.getElementById('question-box')
 const startButton = document.getElementById('startBtn')
-const question= document.getElementById('questions')
+const questionEl= document.getElementById('questionLine')
+const question= document.getElementById('question')
+const answrBtnEL= document.getElementById('answerBtns')
+
 startButton.addEventListener('click', startQuiz)
+
+//List of questions and answers
+const questions = [
+    {
+        question:'Is coding fun?'
+       // answers: [
+         //   {text: 'Yes', correct: true}
+           // {text: 'No', correct: false}
+            //{text: 'Definately No', correct: false}
+           // {text: 'A little bit No', correct: false}
+       // ]
+    }
+]
 
 
 
 function startQuiz(){
     startButton.classList.add('hide')
     questionBox.classList.remove('hide')
-    question.classList.remove('hide')
-
-    
+    questionEl.classList.remove('hide')
+    viewQuestion = questions
+    currentQuestion = 0
+    createNextQuestion() 
 
 }
+
+function createNextQuestion(){
+    showQuestions(questions) 
+        questionEl.innerText =questions
+    }
+
+function showQuestions(question){
+
+}
+
+
 
