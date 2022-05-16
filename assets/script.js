568e47456f3e2c19d9c53bad7c4dc68f3752445e
mbgcodingcrash <re4u2c@gmail.com>
2022-05-15 23:05:27 -0400

    worked more on the clock counter

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 //Variables for the quiz
 var startTime = 60;
+let timerEl= document.getElementById('timer')
 const questionBox =document.getElementById('question-box')
 const startButton = document.getElementById('startBtn')
 const questionEl= document.getElementById('questionLine')
@@ -8,57 +9,69 @@ const answerBtnEL= document.getElementsByClassName('answer')
 
 startButton.addEventListener('click', startQuiz)
 
-//List of questions and answers
-const questionsArray = [ 
-        {
-        question: 'Is coding fun?', 
-        answers: [
-            {text: 'Yes'},
-            {text:'No'},
-            {text:'Maybe'},
-            {text:'Definately Not'}
-        ]},
-        {
-            question: 'What is your favorite language', 
-            answers:[
-            {text: 'Cheesecake'},
-            {text:'JavaScript'},
-            {text:'Pie'},
-            {text:'Definately Not Pie'}
-            ]},
-
-        {question: 'Why are coding',
-        answers: [
-            {text: 'Make all the Money'},
-            {text:'Lose all the Money'},
-            {text:'MaWahahahah Not'},
-            {text:'Definately Not'}
-        ]},
-        {
-            question: 'Did this make your head hurt?',
-            answers: [
-            {text: 'Yes'},
-            {text:'No'},
-            {text:'Maybe'},
-            {text:'Definately Not'}
-            ]   
-        }
-]
 
 
-
+//start the quizz function
 function startQuiz(){
     startButton.classList.add('hide')
     questionBox.classList.remove('hide')
     questionEl.classList.remove('hide')
     createNextQuestion() 
+    countdown()
 
 }
-
+//How to display new questions
 function createNextQuestion(){
         questionEl.innerText =questionsArray
         //answerBtnEL.innerText= questionsArray.forEach 
     }
 
+//Countdown Function
+var countdown =function (){
+    timerEl.innerText=startTime
+    startTime--;
+    if(startTime ===0){
+        clearInterval(startCountdown)
+    }
+}
+var startCountdown =setInterval(countdown, 1000);
 
+
+
+//List of questions and answers
+const questionsArray = [ {
+    
+    question: 'Is coding fun?',
+    answers: [
+        {text: 'Yes'},
+        {text:'No'},
+        {text:'Maybe'},
+        {text:'Definately Not'}
+    ]},
+    {
+        question: 'What is your favorite language', 
+        answers:[
+        {text: 'Cheesecake'},
+        {text:'JavaScript'},
+        {text:'Pie'},
+        {text:'Definately Not Pie'}
+        ]},
+
+    {question: 'Why are coding',
+    answers: [
+        {text: 'Make all the Money'},
+        {text:'Lose all the Money'},
+        {text:'MaWahahahah Not'},
+        {text:'Definately Not'}
+    ]},
+    {
+        question: 'Did this make your head hurt?',
+        answers: [
+        {text: 'Yes'},
+        {text:'No'},
+        {text:'Maybe'},
+        {text:'Definately Not'}
+        ]   
+    }
+]
 
